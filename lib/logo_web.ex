0a5a519c0c8eb59447230a22b5ab0083efaa045b
Sean Huggins <seanhugg@gmail.com>
2024-01-20 14:13:58 -0800

    keyboard events

--- lib/logo_web.ex
+++ lib/logo_web.ex
@@ -51,8 +51,7 @@ defmodule LogoWeb do
 
   def live_view do
     quote do
-      use Phoenix.LiveView,
-        layout: {LogoWeb.Layouts, :app}
+      use Phoenix.LiveView
 
       unquote(html_helpers())
     end
--- lib/logo_web/live/control_live.ex
+++ lib/logo_web/live/control_live.ex
@@ -2,14 +2,32 @@ defmodule LogoWeb.ControlLive do
   use LogoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, [])}
+    {:ok, assign(socket, key: "")}
   end
 
-  # def handle_event("test", %{"number" => })
+  def handle_event("press", %{"key" => key}, socket) do
+    IO.inspect(key)
+
+    {:noreply, assign(socket, key: key)}
+  end
+
+  # def handle_event("press", %{"key" => "ArrowRight"}, socket) do
+  #   IO.inspect("Right")
+
+  #   {:noreply, assign(socket, key: "Right")}
+  # end
+
+  def handle_event("press", _, socket) do
+    IO.inspect("Unknown Key")
+
+    {:noreply, assign(socket, key: "")}
+  end
 
   def render(assigns) do
     ~H"""
-    <h1>Hello World</h1>
+    <div id="control" phx-window-keydown="press">
+      <h2><%= @key %></h2>
+    </div>
     """
   end
 end
